74a7b6878d4f75770e8ecce7acfbe5ca17125d40
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-02-20 22:43:53 +0900

    mod clean code

--- src/BFS.kt
+++ src/BFS.kt
@@ -9,20 +9,19 @@ class BreadthFirstSearch(val graph: Node) {
 private fun Node.find(t: Char) : Char? {
     val q: Deque<Node> = ArrayDeque()
     q.add(this)
-    return findRecur3(q, t)
+    return findRecur2(q, t)
 }
-private fun Node.findRecur(q: Deque<Node>, t: Char) : Char? {
+private tailrec fun Node.findRecur(q: Deque<Node>, t: Char) : Char? {
     if (q.isEmpty()) return null
-
     val v = q.pop()
     System.out.println("search:${v.c}")
     if (v.c == t) return v.c
-    else if (v.children.isNotEmpty())
+    else if (v.hasChildren())
         q.addAll(v.children)
     return findRecur(q, t)
 }
 
-private fun Node.findRecur3(q: Deque<Node>, t: Char) : Char? =
+private fun Node.findRecur2(q: Deque<Node>, t: Char) : Char? =
     if (q.isEmpty())  null
     else q.pop().let { current ->
         System.out.println("search:${current.c}")
@@ -30,7 +29,7 @@ private fun Node.findRecur3(q: Deque<Node>, t: Char) : Char? =
             return@let current.c
         if (current.hasChildren())
             q.addAll(current.children)
-        return@let findRecur3(q, t)
+        return@let findRecur2(q, t)
     }
 
 
