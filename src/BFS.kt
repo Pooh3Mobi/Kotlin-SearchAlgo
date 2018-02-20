d44d8afcd48c2374f80010d869557606f0425c03
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-02-20 09:25:33 +0900

    add fun

--- src/BFS.kt
+++ src/BFS.kt
@@ -48,14 +48,43 @@ class BreadthFirstSearch {
 private fun Node.find(t: Char) : Char? {
     val q: Deque<Node> = ArrayDeque()
     q.add(this)
-    return findRecur(q, t)
+    return findRecur3(q, t)
 }
 private fun Node.findRecur(q: Deque<Node>, t: Char) : Char? {
     if (q.isEmpty()) return null
+
     val v = q.pop()
     System.out.println("search:${v.c}")
     if (v.c == t) return v.c
     else if (v.children.isNotEmpty())
         q.addAll(v.children)
     return findRecur(q, t)
+}
+
+private tailrec fun Node.findRecur2(q: Deque<Node>, t: Char) : Char? =
+    if (q.isEmpty())  null
+    else {
+        val v = q.pop()
+        if (v.c == t){
+            v.c
+        } else if (v.children.isNotEmpty()) {
+            q.addAll(v.children)
+        }
+        findRecur2(q, t)
+    }
+
+private fun Node.findRecur3(q: Deque<Node>, t: Char) : Char? =
+    if (q.isEmpty())  null
+    else q.pop().let { current ->
+        System.out.println("search:${current.c}")
+        if (current.c == t)
+            return@let current.c
+        if (current.hasChildren())
+            q.addAll(current.children)
+        return@let findRecur3(q, t)
+    }
+
+
+private fun Node.hasChildren() : Boolean {
+    return this.children.isNotEmpty()
 }
