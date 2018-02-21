b0320eaea24ca2831d985acd6698302eb820b9b0
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-02-22 08:53:27 +0900

    mod to rambda

--- src/DFS.kt
+++ src/DFS.kt
@@ -9,14 +9,8 @@ class DepthFirstSearch(val graph: Node) {
 private fun Node.find(t: Char) : Node? {
     System.out.println("search:${c}")
     if (t == c) return this
-    else  {
-        var node: Node? = null
-        if (children.isNotEmpty()) {
-            for (child in children) {
-                node = child?.find(t)
-                if (node != null) return node
-            }
-        }
-        return null
-    }
+    else return children.asSequence()
+                .map { node -> node?.find(t) }
+                .filterNotNull()
+                .firstOrNull()
 }
