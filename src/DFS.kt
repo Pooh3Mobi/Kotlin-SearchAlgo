27f440f92ced2ab0d64ca41e1c348fdd195132b4
Pooh3Mobi <pooh3.mobi@gmail.com>
2018-02-22 08:57:38 +0900

    replace no mean sequencial

--- src/DFS.kt
+++ src/DFS.kt
@@ -6,11 +6,12 @@ class DepthFirstSearch(val graph: Node) {
     }
 }
 
-private fun Node.find(t: Char) : Node? {
-    System.out.println("search:${c}")
-    if (t == c) return this
-    else return children.asSequence()
-                .map { node -> node?.find(t) }
-                .filterNotNull()
-                .firstOrNull()
-}
+private fun Node.find(t: Char) : Node? =
+        also { System.out.println("search:${c}") }
+                .let {
+                    if (t == c) return this
+                    else return children.asSequence()
+                            .map { node -> node?.find(t) }
+                            .filterNotNull()
+                            .firstOrNull()
+                }
